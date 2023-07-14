1d8bf9c61f5fe6b44073fcf30eed12222803db0f
seong <ree7243@gmail.com>
2023-07-14 22:29:44 +0900

    Item 상속 관계 매핑

--- src/main/java/Practical/entity/item/Item.java
+++ src/main/java/Practical/entity/item/Item.java
@@ -1,11 +1,15 @@
-package Practical.entity;
+package Practical.entity.item;
+
+import Practical.entity.Category;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name = "ITEM_ID")
@@ -59,3 +63,4 @@ public class Item {
         this.stockQuantity = stockQuantity;
     }
 }
+
