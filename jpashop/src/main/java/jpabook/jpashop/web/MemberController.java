21191061d3ad3745970bccf28a605577b6047d7c
seong <ree7243@gmail.com>
2023-07-29 16:22:53 +0900

    상품 수정

--- jpashop/src/main/java/jpabook/jpashop/web/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/web/MemberController.java
@@ -9,8 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,4 +37,13 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
 }
